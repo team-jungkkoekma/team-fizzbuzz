f73733b9808a0931b1683b4848f89cf792f5534d
ulgoon <ulgoon89@gmail.com>
2023-05-02 11:32:27 +0900

    refactor: Do fizzbuzz with 1 if statement
    Do this with some operators.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,5 @@
 for i in range(1, 30+1):
-    if i % 15 == 0:
-        print('fizzbuzz')
-    elif i % 3 == 0:
-        print('fizz')
-    elif i % 5 == 0:
-        print('buzz')
+    if i % 3 == 0 or i % 5 == 0:
+        print('fizz' * (i % 3 == 0) + 'buzz' * (i % 5 == 0))
     else:
         print(f'{i}')
